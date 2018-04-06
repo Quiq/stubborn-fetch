0c8fe1c31ee2fd3a3797999417531f2abb0e8470
Andrew Jenkins <andrew.jenkins@centricient.com>
2018-04-06 11:48:44 -0600

    use prettier in node_modules

--- src/error.js
+++ src/error.js
@@ -1,11 +1,11 @@
-export function StubbornFetchError (type: string, message: ?string, data: Object = {}) {
+export function StubbornFetchError(type: string, message: ?string, data: Object = {}) {
   this.type = type;
   this.message = message || type;
   this.stack = Error().stack;
   this.data = data;
 }
 StubbornFetchError.prototype = Object.create(Error.prototype);
-StubbornFetchError.prototype.name = "StubbornFetchError";
+StubbornFetchError.prototype.name = 'StubbornFetchError';
 StubbornFetchError.prototype.constructor = StubbornFetchError;
 StubbornFetchError.types = {
   TIMEOUT: 'Timeout',
@@ -18,10 +18,12 @@ StubbornFetchError.types = {
 
 export const ErrorFactory = {
   TIMEOUT: () => new StubbornFetchError(StubbornFetchError.types.TIMEOUT),
-  MAX_ERRORS_EXCEEDED: (errorLimit: number) => new StubbornFetchError("Max_Errors_Exceeded", null, {errorLimit}),
-  NETWORK_ERROR: (underlyingError: Error) => new StubbornFetchError("Network", null, {underlyingError}),
-  STUBBORN_FETCH_DISABLED: () => new StubbornFetchError("Stubborn_Fetch_Disabled"),
-  HTTP_ERROR: (response: Response) => new StubbornFetchError("HTTP", null, {response}),
+  MAX_ERRORS_EXCEEDED: (errorLimit: number) =>
+    new StubbornFetchError('Max_Errors_Exceeded', null, {errorLimit}),
+  NETWORK_ERROR: (underlyingError: Error) =>
+    new StubbornFetchError('Network', null, {underlyingError}),
+  STUBBORN_FETCH_DISABLED: () => new StubbornFetchError('Stubborn_Fetch_Disabled'),
+  HTTP_ERROR: (response: Response) => new StubbornFetchError('HTTP', null, {response}),
   RATE_LIMITED: () => new StubbornFetchError('Rate_Limited'),
 };
 
